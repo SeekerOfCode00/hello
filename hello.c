4b14c7e941c2172ff9e1f3ea5de2f6b22dd36673
Fisher Cyza <fcyza2@huskers.unl.edu>
2024-08-29 11:20:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Fisher Cyza & Julia Clausen
+ * Date: 8/29/2024
+ *
+ * Language: C
+ * Objective: Simple program that says my name.
+ *
+ * Version: 1.0
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Fisher Cyza!\n");
+
+    return(0);
+
+}
